bc6066d330036dd0e497c58fae0788f0cd4e4e8f
Freddy Amarante <freddyamarante14@gmail.com>
2023-04-19 19:19:25 -0400

    fix: jumps midair restricted

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,14 +1,24 @@
-import { RigidBody } from '@react-three/rapier'
+import { useRapier, RigidBody } from '@react-three/rapier'
 import { useFrame } from '@react-three/fiber'
 import { useKeyboardControls } from '@react-three/drei'
 import { useRef, useEffect } from 'react'
 
 export default function Player() {
   const body = useRef()
+  const { rapier, world } = useRapier()
+  const rapierWorld = world.raw()
   const [subscribeKeys, getKeys] = useKeyboardControls()
 
   const jump = () => {
-    body.current.applyImpulse({ x: 0, y: 0.5, z: 0 })
+    const origin = body.current.translation()
+    origin.y -= 0.31
+    const direction = { x: 0, y: -1, z: 0 }
+    const ray = new rapier.Ray(origin, direction)
+    const hit = rapierWorld.castRay(ray, 10, true)
+
+    if (hit.toi < 0.15) {
+      body.current.applyImpulse({ x: 0, y: 0.5, z: 0 })
+    }
   }
 
   useEffect(() => {
